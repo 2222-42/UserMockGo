0f216691ab95faf7e1de1a67a17b5436706e34f6
Satoru HANADA <stephen0617@me.com>
2020-09-18 09:56:41 +0900

    chore(user): IUserRepository作って、Serviceに持たせる。

--- domain/service/userService.go
+++ domain/service/userService.go
@@ -1,23 +1,32 @@
 package service
 
 import (
-	user2 "UserMockGo/domain/model/user"
+	"UserMockGo/domain/infrainterface"
+	"UserMockGo/domain/model/user"
 	"math/rand"
 	"time"
 )
 
+type UserService struct {
+	userRepository infrainterface.IUserRepository
+}
+
+func NewUserService(userRepository infrainterface.IUserRepository) UserService {
+	return UserService{
+		userRepository: userRepository,
+	}
+}
+
 //Passwordはこの時点ではいらないかも？
-func CreateUser(email string, password string, passwordConfirmation string) error {
+func (service UserService) CreateUser(email string, password string, passwordConfirmation string) error {
 	// TODO: idの生成
 	rand.Seed(time.Now().Unix())
 	id := rand.Int63n(10000)
 	// TODO: timerを導入する
 	now := time.Now().Unix()
 
-	//user :=
-	user2.NewUser(id, email, password, passwordConfirmation, now)
+	user := user.NewUser(id, email, password, passwordConfirmation, now)
 
-	//infrainterface使ってInsertする
-
-	return nil
+	// TODO: error handling
+	return service.userRepository.Save(user)
 }
