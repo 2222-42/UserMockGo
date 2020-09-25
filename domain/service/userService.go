3b5d429c75cf9c3f3a3cd491a11b4a25466b5316
Satoru HANADA <stephen0617@me.com>
2020-09-25 15:39:42 +0900

    chore(notifier): *

--- domain/service/userService.go
+++ domain/service/userService.go
@@ -11,20 +11,23 @@ import (
 )
 
 type UserService struct {
-	userRepository infrainterface.IUserRepository
-	idGenerator    infrainterface.IUserIdGenerator
-	tokenGenerator infrainterface.IUserTokenGenerator
+	userRepository     infrainterface.IUserRepository
+	idGenerator        infrainterface.IUserIdGenerator
+	tokenGenerator     infrainterface.IUserTokenGenerator
+	activationNotifier infrainterface.IActivationNotifier
 }
 
 func NewUserService(
 	userRepository infrainterface.IUserRepository,
 	idGenerator infrainterface.IUserIdGenerator,
 	tokenGenerator infrainterface.IUserTokenGenerator,
+	activationNotifier infrainterface.IActivationNotifier,
 ) UserService {
 	return UserService{
-		userRepository: userRepository,
-		idGenerator:    idGenerator,
-		tokenGenerator: tokenGenerator,
+		userRepository:     userRepository,
+		idGenerator:        idGenerator,
+		tokenGenerator:     tokenGenerator,
+		activationNotifier: activationNotifier,
 	}
 }
 
@@ -47,7 +50,11 @@ func (service UserService) CreateUser(email userValues.Email, passString userVal
 	}
 	a := user.NewActivation(userId, token, expiresAt)
 
-	return service.userRepository.CreateUserTransactional(u, p, a)
+	if err := service.userRepository.CreateUserTransactional(u, p, a); err != nil {
+		return err
+	}
+
+	return service.activationNotifier.SendEmail(u, a)
 }
 
 func (service UserService) ActivateUser(email userValues.Email, token string) error {
