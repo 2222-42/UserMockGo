ef18e91b03253b92e0cf786c76bc9c6228218175
Satoru HANADA <stephen0617@me.com>
2020-11-06 15:03:17 +0900

    chore(*): remove IsMfaAuthenticated from auth and jwt

--- domain/infrainterface/tokenManager.go
+++ domain/infrainterface/tokenManager.go
@@ -6,7 +6,7 @@ import (
 )
 
 type ITokenManager interface {
-	GenerateToken(u user.User, isMfaAuthenticated bool) (string, error)
+	GenerateToken(u user.User) (string, error)
 	Parse(tokenString string) (authorization.Authorization, error)
 	//RevokeToken(str string) error
 }
